0b0a6ee500ac8617a3d2f170b88b12dea67d6404
Philipp Veit <hallo@philippveit.de>
2019-11-14 08:55:44 +0100

    first try to store users in the database

---
+++ backend/src/main/java/de/kruemelnerd/todo/backend/repository/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package de.kruemelnerd.todo.backend.repository;
+
+import de.kruemelnerd.todo.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Username '" + username + "' was not found in the Database");
+        }
+        return new TodoUserPrincipal(user);
+    }
+}
